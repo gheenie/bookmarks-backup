d3a56d3f6998ddf7242f804a6e0f0b119af0df9f
infoomics <sbx_94@hotmail.com>
2023-07-15 12:28:36 +0100

    Move file opening to src

--- src/bookmarks_backup.py
+++ src/bookmarks_backup.py
@@ -1,2 +1,4 @@
-def parse_href_links():
-    pass
+def parse_href_links(file_path):
+    with open(file_path, 'r') as file:
+        all_text = file.read()
+        print(all_text)
--- test/test_href_parsing.py
+++ test/test_href_parsing.py
@@ -2,7 +2,4 @@ from src.bookmarks_backup import parse_href_links
 
 
 def test_href_links_are_extracted():
-    with open('seeding/seed_html.html', 'r') as file:
-        all_text = file.read()
-        print(all_text)
-        assert 0 == 1
+    assert parse_href_links('seeding/seed_html.html') == 1
