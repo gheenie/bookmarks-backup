8b5651ff508e31271f3d75302f1c9496d195ba91
infoomics <sbx_94@hotmail.com>
2023-07-15 19:48:23 +0100

    Add method to download webpage

--- src/bookmarks_backup.py
+++ src/bookmarks_backup.py
@@ -1,7 +1,13 @@
+from pywebcopy import save_webpage
+
 from src.href_parser import HrefParser
 
 
 def parse_href_links(file_path):
+    """
+    Read from an exported bookmarks HTML file and search for href URLs
+    """
+
     with open(file_path, 'r') as file:
         all_text = file.read()
 
@@ -9,3 +15,20 @@ def parse_href_links(file_path):
         parser.feed(all_text)
 
         return parser.get_href_links()
+
+
+def backup_page(href_links):
+    """
+    Download pages in full from the given URLs
+    """
+
+    download_folder = 'data/'
+
+    for i, url in enumerate(href_links):
+        save_webpage(
+            url,
+            download_folder,
+            project_name=str(i),
+            bypass_robots=True,
+            open_in_browser=False
+        )
