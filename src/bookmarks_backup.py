4a9b940887aef1a3ee5165db4dfefdbd3d71af3d
infoomics <sbx_94@hotmail.com>
2023-07-15 13:16:07 +0100

    Add parsing logic

--- src/bookmarks_backup.py
+++ src/bookmarks_backup.py
@@ -1,4 +1,11 @@
+from src.href_parser import HrefParser
+
+
 def parse_href_links(file_path):
     with open(file_path, 'r') as file:
         all_text = file.read()
-        print(all_text)
+
+        parser = HrefParser()
+        parser.feed(all_text)
+
+        return parser.get_href_links()
