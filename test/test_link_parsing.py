1fded8f3297442332d285dcfea45089264bbf30b
infoomics <sbx_94@hotmail.com>
2023-07-15 12:14:11 +0100

    Read the seed file

--- test/test_link_parsing.py
+++ test/test_link_parsing.py
@@ -2,4 +2,7 @@ from src.bookmarks_backup import parse_html_links
 
 
 def test_html_links_are_extracted():
-    pass
+    with open('seeding/seed_html.html', 'r') as file:
+        all_text = file.read()
+        print(all_text)
+        assert 0 == 1
