a54cb2fa9b5fc8f5ff0ceb7d4748bbb9dfc7ef9a
infoomics <sbx_94@hotmail.com>
2023-07-16 11:36:43 +0100

    Change main logic to file saving

--- src/index.py
+++ src/index.py
@@ -1,7 +1,9 @@
 from src.bookmarks_backup import (
     parse_href_links,
+    save_to_file,
     backup_page
 )
 
 href_links = parse_href_links('seeding/seed_html.html')
-backup_page(href_links)
+save_to_file(href_links, 'data/parsed_hrefs.txt')
+# backup_page(href_links)
