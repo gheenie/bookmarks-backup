1303f77066d96d8ca97d54d319f80d6ac7467a07
infoomics <sbx_94@hotmail.com>
2023-07-16 11:36:14 +0100

    Add file saving function

--- src/bookmarks_backup.py
+++ src/bookmarks_backup.py
@@ -3,18 +3,29 @@ from pywebcopy import save_webpage
 from src.href_parser import HrefParser
 
 
-def parse_href_links(file_path):
+def parse_href_links(exported_bookmarks):
     """
     Read from an exported bookmarks HTML file and search for href URLs
     """
 
-    with open(file_path, 'r') as file:
+    with open(exported_bookmarks, 'r') as file:
         all_text = file.read()
 
         parser = HrefParser()
         parser.feed(all_text)
 
         return parser.get_href_links()
+
+
+def save_to_file(href_links, saved_hrefs):
+    """
+    Save parsed hrefs to a file
+    """
+
+    hrefs_with_newlines = map(lambda x: x + '\n', href_links)
+
+    with open(saved_hrefs, 'w') as file:
+        file.writelines(hrefs_with_newlines)
 
 
 def backup_page(href_links):
